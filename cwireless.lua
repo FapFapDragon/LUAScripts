ec0ceb1c79304cc19f1efdb0770af701b9fe374b
FapfapDragon <48346054+FapfapDragon@users.noreply.github.com>
2021-01-16 18:38:53 -0500

    Added Computers wireless reciever

---
+++ cwireless.lua
@@ -0,0 +1,20 @@
+local modem = peripheral.wrap("top")
+modem.open(2)
+
+function Split(s, delimiter)
+    result = {};
+    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
+        table.insert(result, match);
+    end
+    return result;
+end
+while true do
+    local event, modemSide, senderChannel, 
+    replyChannel, message, senderDistance = os.pullEvent("modem_message")
+    argz = Split(message, " ");
+    if (argz[1] == "get") then
+        shell.run ("get "..argz[2].." "..argz[3])
+    elseif (argz[1] == "dump") then
+        shell.run("dump")
+    end
+end
