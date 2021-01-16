9cc6abdf17db54f6a55af404d5a9c3561124a4f5
FapfapDragon <48346054+FapfapDragon@users.noreply.github.com>
2021-01-16 16:26:22 -0500

    Added inventory check

--- MC/Get.lua
+++ MC/Get.lua
@@ -1,15 +1,22 @@
 local side = 5
 local itemName = arg[1]
 local amount = arg[2]
-test = refinedstorage.getItem({name="minecraft:diamond"})
+test = refinedstorage.getItem({name=itemName})
 
-function printInfo (item)
+function checkStorage (item)
     if (item.count == nil) then
         print("Sorry, we don't have any of that")
-        return
+        return false
+    elseif (item.count < amount) then
+        print("Sorry we only have" .. item.count .. " " .. itemName .. " avalible")
+        return false
+    else 
+        return true
     end
-    print(item.name .. " : " .. item.count)
+    --print(item.name .. " : " .. item.count)
 end
+
+function 
 
 refinedstorage.extractItem({name="minecraft:diamond"}, 2, 1)
 printInfo(test)
